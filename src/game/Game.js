92e346c4ce752e580ef4ef112898c8629bb21ceb
killscreen <killscreen@users.noreply.github.com>
2016-02-21 15:31:33 -0800

    Sketch in game state

--- src/game/Game.js
+++ src/game/Game.js
@@ -1,14 +1,36 @@
 define(['../Observable'], function (Observable) {
   function Game() {
-    var self = this;
+    var self = this,
+      state = {
+        scene: [
+          {
+            position: [ 0, 0 ],
+            radius: 1,
+            velocity: [ 0, 0 ],
+            what: 'you'
+          },
+          {
+            position: [ 5, 3 ],
+            radius: 2,
+            velocity: [ 0, 0 ],
+            what: 'pig'
+          }
+        ],
+        ontology: [
+          {
+            subject: 'fire',
+            object: 'hot',
+            modifiers: [ 'very' ]
+          }
+        ]
+      };
     Observable(function (notify) {
       self.notify = notify;
+      notify(state);
     });
   }
 
   Game.prototype = Object.create(Observable.prototype);
-
-  
 
   return game;
 });
