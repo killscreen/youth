51bb8877ebc06a141f891053bb604012aaaa78b2
killscreen <killscreen@users.noreply.github.com>
2016-02-21 18:10:01 -0800

    Show scene

--- src/ui/MainView.js
+++ src/ui/MainView.js
@@ -3,19 +3,22 @@ define([
   './StaticView',
   './Region',
   './FactsView',
+  './SceneView',
   '../Submutable',
   'zepto',
   'lodash'
-], function (mainHtml, StaticView, Region, FactsView, Submutable, $, _) {
+], function (mainHtml, StaticView, Region, FactsView, SceneView, Submutable, $, _) {
   function MainView(game) {
     StaticView.call(this, mainHtml);
     this.facts = new FactsView(new Submutable(game, 'facts'));
+    this.scene = new SceneView(new Submutable(game, 'scene'));
     this.observe(_.bind(function (elements) {
-      this.pane = new Region($(elements.filter('div')[0]));
-      this.pane.show(this.facts);
-      // this.canvas = $(this.elements()).find('canvas');
-      // this.ul = new Region($(this.elements()).find('ul')[0]);
-      // this.ul.show(this.ontology);
+      this.panes = [
+        new Region($(elements.filter('div')[0])),
+        new Region($(elements.filter('div')[1]))
+      ];
+      this.panes[0].show(this.scene);
+      this.panes[1].show(this.facts);
     }, this));
   }
 
--- src/ui/SceneView.js
+++ src/ui/SceneView.js
@@ -1,10 +1,33 @@
-define([], function () {
+define([
+  './StaticView',
+  'text!./scene.html',
+  'zepto'
+], function (StaticView, sceneHtml, $) {
   function SceneView(scene) {
-    var context = canvas.getContext('2d');
-    scene.observe(function (entities) {
+    var canvas, context;
 
+    StaticView.call(this, sceneHtml);
+
+    this.unobserve = this.observe(function (elements) {
+      canvas = $(elements).filter('canvas')[0];
+      context = canvas.getContext('2d');
+    });
+
+    scene.observe(function (entities) {
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.strokeStyle = "black";
+      context.fillStyle = "black";
+      entities.map(function (entity) {
+        context.strokeText(
+          entity.what,
+          entity.position[0] * 32,
+          entity.position[1] * 32
+        );
+      });
     });
   }
+
+  SceneView.prototype = Object.create(StaticView.prototype);
 
   return SceneView;
 });
