48957c423cbc56a5b78115f1aba78636c762aa2e
killscreen <killscreen@users.noreply.github.com>
2016-02-27 13:01:54 -0800

    Add ontology lookup table

--- src/game/ontology/Ontology.js
+++ src/game/ontology/Ontology.js
@@ -1,13 +1,18 @@
 define([], function () {
   function Ontology(facts) {
     this.facts = this.facts;
+    this.table = {};
   }
 
   Ontology.prototype.is = function (subject, object) {
-    return this.facts.reduce(function (value, fact) {
-      var is = fact.is(subject, object, this);
-      return is !== undefined ? is : value;
-    }.bind(this), undefined);
+    this.table[subject] = this.table[subject] || {};
+    if (!this.table[subject].hasOwnProperty(object)) {
+      this.table[subject][object] = this.facts.reduce(function (value, fact) {
+        var is = fact.is(subject, object, this);
+        return is !== undefined ? is : value;
+      }.bind(this), undefined);
+    }
+    return this.table[subject][object];
   };
 
   return Ontology;
