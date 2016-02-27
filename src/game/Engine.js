5f46dc6cdfbe60b46dd0b2ce570fea7091e10c8e
killscreen <killscreen@users.noreply.github.com>
2016-02-27 14:26:10 -0800

    Add verlet integrator

--- src/game/Engine.js
+++ src/game/Engine.js
@@ -4,12 +4,14 @@ define([
   './physics/Player',
   './physics/Wall',
   './physics/Friction',
+  './physics/Verlet',
   './ontology/Fact'
-], function (Momentum, Collision, Player, Wall, Friction, Fact) {
+], function (Momentum, Collision, Player, Wall, Friction, Verlet, Fact) {
   function Engine(game) {
     var player = new Player();
     this.game = game;
     this.subsystems = [
+      new Verlet(),
       player,
       new Momentum(),
       new Collision(),
---
+++ src/game/physics/Verlet.js
@@ -0,0 +1,21 @@
+define([], function () {
+  function Verlet() {
+
+  }
+
+  Verlet.prototype.advance = function (entities, delta) {
+    if (this.prior && delta > 0) {
+      this.prior.forEach(function (position, index) {
+        var entity = entities[index];
+        entity.velocity = entity.position.map(function (v, i) {
+          return (v - position[i]) / delta;
+        });
+      });
+    }
+    this.prior = entities.map(function (entity) {
+      return [entity.position[0], entity.position[1]];
+    });
+  };
+
+  return Verlet;
+});
