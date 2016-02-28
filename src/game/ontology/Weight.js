333c9db347cc2ab000f596f6c39295fc466c6654
killscreen <killscreen@users.noreply.github.com>
2016-02-27 19:04:09 -0800

    Allow completion of third level

--- src/game/ontology/Weight.js
+++ src/game/ontology/Weight.js
@@ -13,10 +13,10 @@ define([], function () {
   }
 
   Weight.prototype.weight = function () {
-    return this.modifiers.reduce(function (product, modifier) {
+    return Math.max(this.modifiers.reduce(function (product, modifier) {
       var w = WEIGHTS[modifier];
       return (w === undefined ? 1 : w) * product;
-    }, 1);
+    }, 1), WEIGHTS.extremely * WEIGHTS.extremely);
   };
 
   return Weight;
